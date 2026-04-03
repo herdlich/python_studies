7521b3394d0f396b26084d9b80fc002a3de17980
herdlich <herdlich@gmail.com>
2026-04-03 18:49:28 +0300

    feat: added the "Sum Last Digits" program

---
+++ 03_loops/sum_lasts/sum_lasts_digit.py
@@ -0,0 +1,11 @@
+n = int(input('Enter number: '))
+while n > 9:
+    sum = 0
+    while n > 0:
+        last_digit = n % 10
+        sum += last_digit
+        n //= 10
+    else:
+        n = sum
+else:
+    print(n)
