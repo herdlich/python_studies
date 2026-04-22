0a9f21f56818f27f5a5fecf16af6cc1f881b4543
herdlich <herdlich@gmail.com>
2026-04-22 16:03:19 +0300

    feat: add lists exercises to 02_lists_functions The script returns True if the number is prime

---
+++ 02_lists_functions/prime_number/prime_number.py
@@ -0,0 +1,15 @@
+def is_prime(num):
+    count = 0
+
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
+
+    if count == 2:
+        return True
+    else:
+        return False
+
+
+n = int(input())
+print(is_prime(n))
