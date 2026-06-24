657a4efd9901d853c9ab8d4bf931e8a310001a19
herdlich <herdlich@gmail.com>
2026-06-24 09:05:42 +0300

    feat(books-parser): add logging

--- projects/books_parser/main.py
+++ projects/books_parser/main.py
@@ -1,4 +1,5 @@
 import csv
+import logging
 import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
@@ -7,6 +8,12 @@ from urllib.parse import urljoin
 path_result = Path("result.csv")
 
 BASE_URL = "https://books.toscrape.com/catalogue/"
+
+Path("logs").mkdir(exist_ok=True)
+
+time_format = "%Y-%m-%d %H:%M:%S"
+logging.basicConfig(level=logging.INFO, filename="logs/parser.log", encoding="utf-8",
+                    format="[%(asctime)s] - %(levelname)s: %(message)s", datefmt=time_format)
 
 
 def download_html(url):
@@ -19,15 +26,18 @@ def download_html(url):
 
         if response.status_code == 200:
             html_text = response.text
+            logging.info(f"Page downloaded: {url}, status code: {response.status_code}")
 
             return html_text
 
         else:
             print(f"Page was not downloaded: {url}")
+            logging.warning(f"Page was not downloaded: {url}, status code: {response.status_code}")
             return False
 
     except requests.RequestException as error:
         print(f"Request error: {error}")
+        logging.error(f"Request error: {error}")
         return False
 
 
@@ -104,7 +114,7 @@ def parse_all_pages():
     all_books = []
 
     pages_count = 0
-    for page_number in range(1, 51):
+    for page_number in range(1, 5):
         url = f"https://books.toscrape.com/catalogue/page-{page_number}.html"
 
         html_text = download_html(url)
@@ -116,16 +126,24 @@ def parse_all_pages():
 
         all_books.extend(books_list)
 
+        logging.info(f"Parsed books from page {page_number}: {len(books_list)}")
+
         pages_count += 1
 
     if not all_books:
         print("No books found")
+        logging.info("No books found")
         return
+
+    save_csv(path_result, all_books)
 
     print(f"Pages processed: {pages_count}")
     print(f"Books saved: {len(all_books)}")
 
-    save_csv(path_result, all_books)
+    logging.info(f"Pages processed: {pages_count}")
+    logging.info(f"Books saved: {len(all_books)}")
+
+    logging.info(f"CSV saved: {path_result}")
 
 
 parse_all_pages()
